3565a896b6556c24b6ca0ef7a939d3989bc4389a
amsahu <60923910+amsahu@users.noreply.github.com>
2021-05-20 18:12:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+from spacy import load
+
+app = Flask(__name__)
+model = load('model_spacy')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    text = str(request.form['text'])
+    doc = model(text)
+    
+    l=[(X.text, X.label_) for X in doc.ents]
+    
+    if len(l)>0:
+        label = l[0][1]
+    else:
+        label = 'None'
+
+    output = label
+
+    return render_template('index.html', prediction_text='Entity:  {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
